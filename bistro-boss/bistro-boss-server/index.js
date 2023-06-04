17b805134da0692f0735be38f16b8506a801058e
RISHAD-prog <20-43002-1@student.aiub.edu>
2023-06-04 23:56:18 +0600

    added admin route and validate JWT

--- bistro-boss/bistro-boss-server/index.js
+++ bistro-boss/bistro-boss-server/index.js
@@ -2,11 +2,27 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+        return res.status(401).send({ error: true, message: 'unauthorized access' });
+    }
+    // bearer token
+    const token = authorization.split(' ')[1];
 
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ error: true, message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.vv6xnmz.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -18,7 +34,6 @@ const client = new MongoClient(uri, {
         deprecationErrors: true,
     }
 });
-
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
@@ -27,6 +42,15 @@ async function run() {
         const ReviewDB = client.db("Bistro-BossDB").collection("reviews");
         const addCartDB = client.db("Bistro-BossDB").collection("carts");
         const UserDB = client.db("Bistro-BossDB").collection("user");
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+            res.send({ token });
+        })
+        app.get("/users", verifyJWT, async (req, res) => {
+            const result = await UserDB.find().toArray();
+            res.send(result);
+        })
         app.post("/users", async (req, res) => {
             const user = req.body;
             const query = { email: user?.email, name: user?.name };
@@ -37,6 +61,30 @@ async function run() {
             const result = await UserDB.insertOne(user);
             return res.send(result);
         })
+        app.patch("/users/admin/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const updateDoc = {
+                $set: {
+                    role: 'admin',
+                },
+            };
+            const result = await UserDB.updateOne(query, updateDoc);
+            res.send(result);
+        });
+        app.get("/users/admin/:email", verifyJWT, async (req, res) => {
+            const email = req.params.email;
+
+            if (req.decoded.email !== email) {
+                res.send({ admin: false })
+            }
+
+            const query = { email: email }
+            const user = await UserDB.findOne(query);
+            const result = { admin: user?.role === 'admin' }
+            res.send(result);
+
+        });
         app.get("/menu", async (req, res) => {
             const result = await MenuDB.find().toArray();
             res.send(result);
@@ -45,22 +93,26 @@ async function run() {
             const result = await ReviewDB.find().toArray();
             res.send(result);
         });
-        app.post("/cart", async (req, res) => {
+        app.post("/carts", async (req, res) => {
             const cartData = req.body;
             const result = await addCartDB.insertOne(cartData);
             res.send(result);
         });
-        app.get("/cart", async (req, res) => {
+        app.get("/carts", verifyJWT, async (req, res) => {
             const userEmail = req.query.email;
             console.log(userEmail);
             if (!userEmail) {
                 res.send([]);
             }
+            const decodedEmail = req.decoded.email;
+            if (decodedEmail !== userEmail) {
+                return res.status(403).send({ error: true, message: 'forbidden access' })
+            }
             const query = { email: userEmail };
             const result = await addCartDB.find(query).toArray();
             res.send(result);
         });
-        app.delete("/cart/:id", async (req, res) => {
+        app.delete("/carts/:id", async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) };
             const result = await addCartDB.deleteOne(query);
